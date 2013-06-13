30a827962d01822ed75e055e52b4e17054b0d0f3
Aaron Weatherall <aaron@4mation.com.au>
2013-06-14 08:50:44 +1000

    Adding schema to module

---
+++ module/News/data/schema.mysql.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `news` (
+  `newsId` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  `content` text,
+  `author` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`newsId`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `display_name` varchar(50) DEFAULT NULL,
+  `password` varchar(128) NOT NULL,
+  `state` smallint(5) unsigned DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
